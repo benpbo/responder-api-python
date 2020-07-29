e841e56d69d3d8050b10846e19b57209a86b1bc7
ben <prizeben@gmail.com>
2020-07-29 17:17:48 +0300

    added views getter and fixed a bug

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from responder_api_handler import ResponderClient
 import os
 from dotenv import load_dotenv
 import json
+import bs4
 
 load_dotenv()
 c_key = os.getenv("CLIENT_KEY")
@@ -10,8 +11,10 @@ u_key = os.getenv("USER_KEY")
 u_secret = os.getenv("USER_SECRET")
 client = ResponderClient(c_key, c_secret, u_key, u_secret)
 
-content = client.get_messages(731735)
-if (content is None):
-    print('no respone')
+content = client.get_lists()
+
+lists = [lst['ID'] for lst in content['LISTS']]
+views = [client.get_views(list_id) for list_id in lists]
+
 with open('.tmp', 'w', encoding='utf8') as f:
-    f.write(json.dumps(content, indent=2, ensure_ascii=False))
+    f.write(json.dumps(views, ensure_ascii=False, indent=2))
--- responder_api_handler.py
+++ responder_api_handler.py
@@ -34,16 +34,22 @@ class OAuth:
 class ResponderClient:
     lists_url = 'http://api.responder.co.il/main/lists'
     messages_url_template = Template('http://api.responder.co.il/main/lists/$listId/messages/$messageId')
+    views_url_template = Template('http://api.responder.co.il/main/lists/$listId/views')
 
     def __init__(self, c_key, c_secret, u_key, u_secret):
         self.oauth = OAuth(c_key, c_secret, u_key, u_secret)
-        self.auth_header = self.oauth.generate_auth_header()
+        self.auth_header = self.oauth.generate_auth_header
 
-    def get_lists(self) -> dict:
-        res = requests.get(ResponderClient.lists_url, headers=self.auth_header)
+    def get_lists(self):
+        res = requests.get(ResponderClient.lists_url, headers=self.auth_header())
         return json.loads(res.content)
 
-    def get_messages(self, list_id, message_id='') -> dict:
-        url = self.messages_url_template.substitute(listId=list_id, messageId=message_id)
-        res = requests.get(url, headers=self.auth_header)
+    def get_messages(self, list_id, message_id=''):
+        url = ResponderClient.messages_url_template.substitute(listId=list_id, messageId=message_id)
+        res = requests.get(url, headers=self.auth_header())
+        return json.loads(res.content)
+
+    def get_views(self, list_id):
+        url = ResponderClient.views_url_template.substitute(listId=list_id)
+        res = requests.get(url, headers=self.auth_header())
         return json.loads(res.content)
