1481b65f00ad9d4123b19a6788a00ce406dc51b0
ben <prizeben@gmail.com>
2020-07-30 17:23:50 +0300

    added update message function

--- main.py
+++ main.py
@@ -18,7 +18,33 @@ lst = {
     'SENDER_ADDRESS': 'somewhere'
 }
 
-data = client.create_list(lst)
+msg = {
+    "TYPE": "1", # 1 for "messer boded", 0 for "sidrat messarim", 2 for "mevusas ta'arich"
+    "BODY_TYPE": "0", # 0 for regular HTML editor(affects the type of editor used in the website)
+    "SUBJECT": 'message subject2',
+    "BODY": 'message HTML body',
+    "FILTER": "", # -optional - the view id "kvuzat mishloah"
+    'LANGUAGE': "hebrew", # -optional - defaults to 'english'
+    'CHECK_LINKS': "1"
+}
+msg_update = {
+    'LANGUAGE': 'english'
+}
+list_id = 0
+for lst in json.loads(client.get_lists())['LISTS']:
+    if lst['DESCRIPTION'] == 'test':
+        list_id = lst['ID']
+        break
+
+#list_id = json.loads(client.create_list(lst))['LIST_ID']
+#data = client.create_message(list_id, msg)
+
+message_id = 0
+for msg in json.loads(client.get_messages(list_id)):
+    if msg['SUBJECT'] == 'test message':
+        message_id = msg['ID']
+    
+data = client.update_message(list_id, message_id, msg_update)
 try:
     json = json.dumps(json.loads(data), indent=2, ensure_ascii=False)
     print(json)
--- responder_api_handler.py
+++ responder_api_handler.py
@@ -3,14 +3,17 @@ import requests
 from string import Template
 from OAuth import OAuth
 
+
 class ResponderClient:
-    lists_url_template = Template('http://api.responder.co.il/main/lists/$listId')
-    messages_url_template = Template('http://api.responder.co.il/main/lists/$listId/messages/$messageId/')
-    views_url_template = Template('http://api.responder.co.il/main/lists/$listId/views/')
+    lists_url_template = Template(
+        'http://api.responder.co.il/main/lists/$listId')
+    messages_url_template = Template(
+        'http://api.responder.co.il/main/lists/$listId/messages/$messageId/')
+    views_url_template = Template(
+        'http://api.responder.co.il/main/lists/$listId/views/')
 
     def __init__(self, c_key, c_secret, u_key, u_secret):
         self.oauth = OAuth(c_key, c_secret, u_key, u_secret)
-        self.auth_header = self.oauth.generate_auth_header
 
     def get_lists(self):
         url = ResponderClient.lists_url_template.substitute(listId='')
@@ -19,22 +22,35 @@ class ResponderClient:
 
     def create_list(self, data):
         url = ResponderClient.lists_url_template.substitute(listId='')
-        res = self.oauth.make_request(url, method='POST', data=data)
+        res = self.oauth.make_request(url, 'POST', data=data)
         return res.content
-    
+
     def update_list(self, list_id, data):
         url = ResponderClient.lists_url_template.substitute(listId=list_id)
-        res = self.oauth.make_request(url, method='PUT', data=data)
+        res = self.oauth.make_request(url, 'PUT', data=data)
         return res.content
 
     def delete_list(self, list_id):
         url = ResponderClient.lists_url_template.substitute(listId=list_id)
-        res = self.oauth.make_request(url, method='DELETE')
+        res = self.oauth.make_request(url, 'DELETE')
         return res.content
 
     def get_messages(self, list_id, message_id=''):
-        url = ResponderClient.messages_url_template.substitute(listId=list_id, messageId=message_id)
+        url = ResponderClient.messages_url_template.substitute(
+            listId=list_id, messageId=message_id)
         res = self.oauth.make_request(url)
+        return res.content
+
+    def create_message(self, list_id, data):
+        url = ResponderClient.messages_url_template.substitute(
+            listId=list_id, messageId='')
+        res = self.oauth.make_request(url, 'POST', data=data)
+        return res.content
+
+    def update_message(self, list_id, message_id, data):
+        url = ResponderClient.messages_url_template.substitute(
+            listId=list_id, messageId=message_id)
+        res = self.oauth.make_request(url, 'PUT', data=data)
         return res.content
 
     def get_views(self, list_id):
